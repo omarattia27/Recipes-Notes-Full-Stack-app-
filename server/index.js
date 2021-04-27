b3b1b4457b050c0fa7fd2a1eeea207f6c11cb0d4
omarattia27 <70349770+omarattia27@users.noreply.github.com>
2021-04-27 01:29:56 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({  extended: true }))
 app.use(cors());
 app.use('/api', api);
 
-const CONNECTION_URL = "mongodb+srv://Omarmohamed2013:Omarmohamed2013@cluster0.0ffel.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = "mongodb+srv://<Name>:<Password>@cluster0.0ffel.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
